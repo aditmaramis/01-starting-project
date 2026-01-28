2c3d014405355d3a8fed31baa34eda7e133020b9
aditmaramis <adit.maramis@gmail.com>
2026-01-28 13:58:46 +0700

    Add TabButton component and integrate it into App for dynamic tab selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Header from './components/Header/Header.jsx';
 import { CORE_CONCEPTS } from './data.js';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 
 function App() {
+	function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+		console.log(`Tab selected: ${selectedButton}`);
+	}
+
 	return (
 		<div>
 			<Header />
@@ -10,21 +16,22 @@ function App() {
 				<section id="core-concepts">
 					<h2>Core Concepts</h2>
 					<ul>
-						<CoreConcept
-							{...CORE_CONCEPTS[0]}
-						/>
-						<CoreConcept
-							{...CORE_CONCEPTS[1]}
-						/>
-						<CoreConcept
-							{...CORE_CONCEPTS[2]}
-						/>
-						<CoreConcept
-							{...CORE_CONCEPTS[3]}
-						/>
+						<CoreConcept {...CORE_CONCEPTS[0]} />
+						<CoreConcept {...CORE_CONCEPTS[1]} />
+						<CoreConcept {...CORE_CONCEPTS[2]} />
+						<CoreConcept {...CORE_CONCEPTS[3]} />
 					</ul>
 				</section>
-				<h2>Time to get started!</h2>
+				<section id="examples">
+					<h2>Examples</h2>
+					<menu>
+						<TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+						<TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+						<TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+						<TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+					</menu>
+					Dynamic Content
+				</section>
 			</main>
 		</div>
 	);
